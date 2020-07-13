d14bdd7b7a07363c7c189e0ca5f623909389a16a
Feras Maali <ferasn.maali@gmail.com>
2020-07-13 12:24:42 +0300

    Created basic project structure

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3
+RUN mkdir /code
+COPY requirements.txt /code/
+WORKDIR /code
+RUN pip3 install -r requirements.txt
+VOLUME ["/code"]
+# EXPOSE
---
+++ src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flaskext.mysql import MySQL
+import os, json
+
+app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'example'
+app.config['MYSQL_DATABASE_DB'] = 'my_db'
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+
+mysql.init_app(app)
+
+@app.route('/')
+def hello():
+    return 'hi there'
+
+@app.route('/data')
+def data():
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * from data")
+    return json.dumps(cursor.fetchall())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
