8320d70f39d67771b003ae6a55176d017a6f2063
Feras Maali <ferasn.maali@gmail.com>
2020-07-14 07:36:46 +0300

    Fixed indentation

--- src/app.py
+++ src/app.py
@@ -15,16 +15,16 @@ sql_connect = mysql.connect()
 
 @app.route('/')
 def hello():
-    return 'hi there'
+	return 'hi there'
 
 @app.route('/cpu')
 def data():
-    cursor = sql_connect.cursor()
-    cursor.execute(
+	cursor = sql_connect.cursor()
+	cursor.execute(
 		''' SELECT * FROM cpu_usage
-			WHERE taken_at >= DATE_SUB(NOW(), INTERVAL 7 DAY);
+		WHERE taken_at >= DATE_SUB(NOW(), INTERVAL 7 DAY);
 		''')
 	return json.dumps(cursor.fetchall(), indent=4, sort_keys=True, default=str)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+	app.run(host="0.0.0.0", debug=True)
