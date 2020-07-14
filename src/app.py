a9c672c521193ea467e5d9abd94ee71c886eabdc
Feras Maali <ferasn.maali@gmail.com>
2020-07-14 07:25:22 +0300

    Completed /cpu API

--- src/app.py
+++ src/app.py
@@ -11,15 +11,19 @@ app.config['MYSQL_DATABASE_DB'] = 'performance_api_app'
 app.config['MYSQL_DATABASE_HOST'] = 'db'
 
 mysql.init_app(app)
+sql_connect = mysql.connect()
 
 @app.route('/')
 def hello():
     return 'hi there'
 
-@app.route('/data')
+@app.route('/cpu')
 def data():
-    cursor = mysql.connect().cursor()
-    cursor.execute("SELECT * from cpu_usage")
+    cursor = sql_connect.cursor()
+    cursor.execute(
+		''' SELECT * FROM cpu_usage
+			WHERE taken_at >= DATE_SUB(NOW(), INTERVAL 7 DAY);
+		''')
 	return json.dumps(cursor.fetchall(), indent=4, sort_keys=True, default=str)
 
 if __name__ == "__main__":
